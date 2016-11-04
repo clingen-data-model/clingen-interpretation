286a59c3a411e6fb748c6166892d93a85f17503d
Bradford Powell <bpow@drpowell.org>
2016-11-04 15:33:00 -0400

    preliminary code to reformat examples
    relevant to #6, but not done yet
    
    This currently outputs as yaml because all internal references are
    maintained (including circular references), which is not supported
    by standard JSON (although we could consider using JSON references)

---
+++ reformat_examples.rb
@@ -0,0 +1,98 @@
+#!/usr/bin/ruby
+
+require 'json'
+require 'yaml'
+
+$dmwgExamples = Hash.new { |hash, key| hash[key] = {} }
+
+$flattened = Hash[Dir["data/flattened/*.json"].collect { |jsonfile|
+	[File.basename(jsonfile, '.json'), JSON.parse(File.read(jsonfile))]
+}]
+
+#puts $flattened.to_yaml
+
+$entity_attributes = Hash.new { |hash, key| hash[key] = [] }
+$flattened['Attribute'].each do |a_id, a_rec|
+	$entity_attributes[a_rec['entityId']].push(a_rec)
+end
+
+$entity_attributes.each do |e_id, a_recs|
+	a_recs.sort_by! { |x| x['precedence'] || 0}
+end
+
+def convert_value(value, type)
+	case type
+	when 'String'
+		value
+	when 'int'
+		value.to_i
+	when 'float'
+		value.to_f
+	when 'boolean'
+		!!value
+	when '???'
+		value
+	when 'CodeableConcept'
+		# FIXME
+		value
+	else
+		$dmwgExamples[value]
+	end
+end
+
+$flattened['Entity'].each do |e_id, e_rec|
+	e_name = e_rec['name']
+	STDERR.puts "#{e_id}, #{e_name}"
+	if e_name === 'Entity' then next end
+	## FIXME Data sheet represents many different entities
+	## consider moving Data.explanation to DataAttribute table
+	if e_name === 'Data' then next end
+	if $flattened.key? e_name then
+		# full fledged table for this entity (not a Data subtype)
+		STDERR.puts "### has a full table!"
+		$flattened[e_name].each do |id, rec|
+			ex = $dmwgExamples[id]
+			ex['@id'] = id
+			$entity_attributes[e_id].each do |attribute|
+				if rec.key? attribute['name'] then
+					ex[attribute['name']] = convert_value(rec[attribute['name']], attribute['dataType'])
+				end
+			end
+			# handle inherited attributes
+			parent = e_rec['parentEntityTypeId']
+			while !!parent
+				$entity_attributes[parent].each do |attribute|
+					if rec.key? attribute['name'] then
+						ex[attribute['name']] = convert_value(rec[attribute['name']], attribute['dataType'])
+					end
+				end
+				parent = $flattened['Entity'][parent]['parentEntityTypeId']
+			end
+		end
+	else
+		# try the Data and DataAttribute tables
+		STDERR.puts "### no table for this one!"
+	end
+end
+
+$flattened['DataAttribute'].each do |k, v|
+	# FIXME - make sure inherited attributes are appropriately handled
+	attribute = $flattened['Attribute'][v['attributeId']]
+	$dmwgExamples[v['evidenceDataId']]['@id'] = v['evidenceDataId']
+	$dmwgExamples[v['evidenceDataId']][attribute['name']] = convert_value(v['value'], attribute['dataType'])
+end
+
+# TODO: joins using ActivityAgentAssociation, ActivityUsedEntity
+# consider merging this code with DataAttribute handling
+
+$flattened['ActivityAgentAssociation'].each do |aaa|
+	activity = $dmwgExamples[aaa['activityId']]
+	(activity['wasAssociatedWith'] ||= []).push({ 'agent' => aaa['wasAssociatedWith'], 'role' => $flattened['Attribute'][aaa['roleAttributeId']]['name']})
+end
+
+$flattened['ActivityUsedEntity'].each do |aue|
+	activity = $dmwgExamples[aue['activityId']]
+	((activity['used'] ||= {})[$flattened['Entity'][aue['usedEntityType']]['name']] ||= []).push($dmwgExamples[aue['usedEntityId']])
+end
+
+puts $dmwgExamples['VarInterp001'].to_yaml
