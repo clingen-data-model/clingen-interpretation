3c5c94dc2b3d46450dfac8919aaf5e1b13ad4948
Bradford Powell <bpow@drpowell.org>
2017-06-20 11:39:43 -0400

    modifications for data name changes

--- misc/CritAssess_summary.rb
+++ misc/CritAssess_summary.rb
@@ -8,20 +8,19 @@ assessments_by_criterion =
     |x| x.key?('criterion') && x.key?('outcome')
   }.group_by { |x| x['criterion']['id'] }
 
-puts "Criterion\tMet\tInsufficient\tRefuted\tTotal"
+puts "Criterion\tMet\tNot Met\tTotal"
 
 assessments_by_criterion.each do |crit, cas|
   data_types = Set.new
   cas.each do |ca|
-    ca.fetch('hasSupportingEvidence', []).each do |evl|
-      evl.fetch('supportingData', []).each { |sd| data_types << sd['type'] }
+    ca.fetch('evidence', []).each do |evl|
+      evl.fetch('information', []).each { |sd| data_types << sd['type'] }
     end
   end
   data_types = data_types.to_a.sort.join ", "
   puts [crit,
-        cas.select {|x| x['outcome'] == 'CG:MET'}.length,
-        cas.select {|x| x['outcome'] == 'CG:INSUFFICIENT'}.length,
-        cas.select {|x| x['outcome'] == 'CG:REFUTE'}.length,
+        cas.select {|x| x['outcome'] == 'CG-criterion-outcome:met'}.length,
+        cas.select {|x| x['outcome'] == 'CG-criterion-outcome:met'}.length,
         cas.length,
         data_types
       ].join "\t"
