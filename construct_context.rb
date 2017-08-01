684fa3fc9bde7b850e15b0bd7f6d01c85db239ca
Bradford Powell <bpow@drpowell.org>
2017-08-01 11:34:45 -0400

    make context construction runnable or "require"able

--- construct_context.rb
+++ construct_context.rb
@@ -3,23 +3,29 @@ require 'json'
 DM_BASE_IRI = "http://datamodel.clinicalgenome.org/types/"
 AR_BASE_IRI = "http://schema.genome.network/"
 
-types = JSON.parse(File.read('data/flattened/Type.json'))
-attributes = JSON.parse(File.read('data/flattened/Attribute.json'))
+def construct_context(data_dir = File.join('data', 'flattened'))
+  types = JSON.parse(File.read(File.join(data_dir, "Type.json")))
+  attributes = JSON.parse(File.read(File.join(data_dir, "Attribute.json")))
 
-cx = {
-        cg: DM_BASE_IRI,
-        base: DM_BASE_IRI,
-        gns: AR_BASE_IRI,
-        id: "@id",
-        type: "@type"
-      }
+  cx = {
+          cg: DM_BASE_IRI,
+          base: DM_BASE_IRI,
+          gns: AR_BASE_IRI,
+          id: "@id",
+          type: "@type"
+        }
 
-attributes.each do |id, attrib|
-  if ['String', 'int', 'boolean', 'float'].include? attrib['dataType']
-    cx[attrib['name']] = "cg:#{attrib['name']}"
-  else
-    cx[attrib['name']] = { "@id" => "cg:#{attrib['name']}", "@type" => "@id" }
+  attributes.each do |id, attrib|
+    if ['String', 'int', 'boolean', 'float'].include? attrib['dataType']
+      cx[attrib['name']] = "cg:#{attrib['name']}"
+    else
+      cx[attrib['name']] = { "@id" => "cg:#{attrib['name']}", "@type" => "@id" }
+    end
   end
+
+  return { "@context" => cx }
 end
 
-print JSON.pretty_generate({ "@context" => cx })
+if __FILE__ == $0
+  print JSON.pretty_generate(construct_context())
+end
